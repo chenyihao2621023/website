8ebc62c2d99754de28bf029e6e668d5ec658d5b3
cyh <372528184@qq.com>
2017-05-09 17:37:14 +0800

    .

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const http = require('http')
 http.createServer(function(req,res) {
   res.writeHead(200,{'Content-Type':'text/plain;charset=utf-8'})
   res.end('hello world')
-}).listen(8081)
+}).listen(3000)
 console.log('server running on http')
